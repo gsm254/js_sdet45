4fcccf2c0289ac1ef5108236c5156f5a0e030b4a
gowtham <gowthams254@yahoo.com>
2023-01-25 17:27:36 +0530

    project added

---
+++ Demo.js
@@ -0,0 +1,9 @@
+var a= 10
+console.log(a);
+var a =30
+console.log(a);
+let b = a+"gowtham"
+console.log(b);
+const c = 1
+console.log(c);
+
---
+++ Sample/TypeOfProg.js
@@ -0,0 +1,22 @@
+
+
+var a = 10;
+console.log(a);
+console.log(typeof(a));//number  for all kind of numbers only one datatype "number"
+
+var b = "gowtham";
+console.log(a);
+console.log(typeof(b));//string
+
+var c = 10.2;
+console.log(c);
+console.log(typeof(c));//boolean
+
+var d = "10";
+console.log(d);
+console.log(typeof(Number(d)));//explicitly typecasting string to number
+
+var e = "aaa";
+console.log(e);
+e = Number(e)
+console.log(typeof(e));
